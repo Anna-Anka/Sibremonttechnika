bea56b17a1f390345eb6478aab722dcc03d61e3e
Anna-Anka <anjbekrenewa@gmail.com>
2022-06-03 12:13:01 +0300

    added adaptive for header

--- app/js/main.js
+++ app/js/main.js
@@ -21,19 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     //Вызов каталога
-
-    // document.querySelector('.header__catalog').onclick = function () {
-    //     document.querySelector('.menu').classList.toggle('menu--active');
-    //     document.querySelector('.header__line').classList.toggle('header__line--active');
-    // };
-
-    // const catalog = document.querySelector('.menu__wrapper');
-    // document.addEventListener('mousedown', (e) => {
-    //     if (!catalog.contains(e.target)) {
-    //         document.querySelector('.menu').classList.remove('menu--active');
-    //     }
-    // });
-
     const btnMenu = document.querySelector('.header__catalog');
     const menu = document.querySelector('.menu');
     const toggleMenu = function () {
@@ -56,4 +43,71 @@ document.addEventListener("DOMContentLoaded", () => {
             toggleMenu();
         }
     });
+
+    //Поиск 
+    const btnSearch = document.querySelector('.header__mobile--search');
+    const search_form = document.querySelector('.search-form');
+    const toggleform = function () {
+        search_form.classList.toggle('search-form--active');
+    }
+
+    btnSearch.addEventListener('click', function (e) {
+        e.stopPropagation();
+        toggleform();
+    });
+
+    document.addEventListener('click', function (e) {
+        const target = e.target;
+        const its_search_form = target == search_form || search_form.contains(target);
+        const its_btnSearch = target == btnSearch;
+        const menu_is_active = search_form.classList.contains('search-form--active');
+
+        if (!its_search_form && !its_btnSearch && menu_is_active) {
+            toggleform();
+        }
+    });
+
+    //Звонок 
+    const btnCall = document.querySelector('.header__mobile--call');
+    const call = document.querySelector('.header__call');
+    const toggleCall = function () {
+        call.classList.toggle('header__call--active');
+    }
+
+    btnCall.addEventListener('click', function (e) {
+        e.stopPropagation();
+        toggleCall();
+    });
+
+    document.addEventListener('click', function (e) {
+        const target = e.target;
+        const its_call = target == call || call.contains(target);
+        const its_btnCall = target == btnCall;
+        const menu_is_active = call.classList.contains('header__call--active');
+
+        if (!its_call && !its_btnCall && menu_is_active) {
+            toggleCall();
+        }
+    });
+
+
+
+
+
+
+
 });
+
+
+    // const mediaQuery = window.matchMedia('(max-width: 992px)')
+
+    // function handleTabletChange(e) {
+    //     if (e.matches) {
+    //         document.querySelector('.search-form__button').onclick = function () {
+    //             document.querySelector('.search-form__item').classList.toggle('search-form__item--active');
+    //         };
+    //     };
+    // };
+
+    // mediaQuery.addListener(handleTabletChange)
+    // handleTabletChange(mediaQuery)
